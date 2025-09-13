0aec27b52fabe5fe45ef38f0ac9fb672586a6807
nandanugg <nandanugg@gmail.com>
2025-09-13 21:56:37 +0700

    modularize

--- src/provider.db.ts
+++ src/provider.db.ts
@@ -20,11 +20,3 @@ export const purchaseCollection = new Datastore({
   filename: "./data/purchases.db",
   autoload: true,
 });
-
-// Ensure unique indexes
-userCollection.ensureIndex({ fieldName: "email", unique: true, sparse: true });
-userCollection.ensureIndex({ fieldName: "phone", unique: true, sparse: true });
-fileCollection.ensureIndex({ fieldName: "fileId", unique: true });
-productCollection.ensureIndex({ fieldName: "productId", unique: true });
-productCollection.ensureIndex({ fieldName: "userId" });
-purchaseCollection.ensureIndex({ fieldName: "purchaseId", unique: true });
